e159e945194f646336fd3a5838d82ac829625e95
DamianL96 <damianlambrecht@gmail.com>
2025-01-17 17:12:09 -0300

    funcion para agregar amigos

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
 
 let amigos = [];
 
+function agregarAmigo(){
+  let amigo = document.querySelector('#amigo').value;
+  
+  if(amigo){
+    amigos.push(amigo);
+    console.log(amigos);
+  }else{
+    alert('Por favor, inserte un nombre');
+  }
+  document.querySelector('#amigo').value = '';
+}
