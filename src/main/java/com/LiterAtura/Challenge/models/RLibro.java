fba0d30c956b28e3244a9766c1c78ad777ee8fbc
DamianL96 <damianlambrecht@gmail.com>
2025-06-17 12:19:13 -0300

    Record Libro creado

---
+++ src/main/java/com/LiterAtura/Challenge/models/RLibro.java
@@ -0,0 +1,12 @@
+package com.LiterAtura.Challenge.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record RLibro(
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") String autor,
+        @JsonAlias("languages") String idiomas,
+        @JsonAlias("download_count") Integer descargas
+){}
